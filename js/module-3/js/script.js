2320f6a0bca7d5ff365e537fb510b3717bdbcd98
Max Shuliak <maxshulia@gmail.com>
2018-02-19 17:27:37 +0200

    hw3

---
+++ js/module-3/js/script.js
@@ -0,0 +1,8 @@
+const alphabet = ["qwertyuiop[]","asdfghjkl;'","zxcvbnm,./"];
+
+const hello = alphabet[0];
+const javascript = alphabet[1];
+const trainer = alphabet[2];
+
+const message = `Перший елемент масиву ${hello},\nДругий елемент масиву ${javascript},\nТретій елемент масиву ${trainer}`;
+console.log(message);
