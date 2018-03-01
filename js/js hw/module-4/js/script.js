323313a3a204ebe9a047004671ef023afe4b6962
Max Shuliak <maxshulia@gmail.com>
2018-03-01 20:11:51 +0200

    hw4 remake

--- js/js hw/module-4/js/script.js
+++ js/js hw/module-4/js/script.js
@@ -1,47 +1,46 @@
 //Task1
 
-// const keyboard = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-//
-//   function addKeyboardLayout( alphabet ){
-//     const upperRow =keyboard.slice(0,12).split(" ");
-//     const midRow =keyboard.slice(12,23).split(" ");
-//     const lowRow =keyboard.slice(23,33).split(" ");
-//
-//     alphabet = [upperRow,midRow,lowRow];
-//     console.log(alphabet);
-//   }
-//
-// addKeyboardLayout( keyboard );
+const keyboard = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+
+  function addKeyboardLayout( alphabet ){
+    const upperRow =keyboard.slice(0,12).split(" ");
+    const midRow =keyboard.slice(12,23).split(" ");
+    const lowRow =keyboard.slice(23,33).split(" ");
+
+    alphabet = [upperRow,midRow,lowRow];
+    console.log(alphabet);
+  }
+
+addKeyboardLayout( keyboard );
 
 //Task2
 
-// const msg = +prompt("введіть число 1-3");
-//
-// function getRandCharInRow(row){
-//   row = "";
-//   const alphabet = ["qwertyuiop[]","asdfghjkl;'","zxcvbnm,./"]
-//
-//
-//     if (msg === 1){
-//       row = alphabet[0].charAt(Math.floor(Math.random()*alphabet[0].length));
-//     } else if (msg === 2){
-//       row = alphabet[1].charAt(Math.floor(Math.random()*alphabet[1].length));
-//     } else if (msg === 3){
-//       row = alphabet[2].charAt(Math.floor(Math.random()*alphabet[2].length));
-//     } else {
-//       console.log("error");
-//     }
-//
-//   return row;
-// }
-// console.log(getRandCharInRow());
+const msg = +prompt("введіть число 1-3");
+
+function getRandCharInRow(row){
+  row = "";
+  const arr = ["qwertyuiop[]","asdfghjkl;'","zxcvbnm,./"]
+
+
+    if (msg === 1){
+      row = arr[0].charAt(Math.floor(Math.random()*arr[0].length));
+    } else if (msg === 2){
+      row = arr[1].charAt(Math.floor(Math.random()*arr[1].length));
+    } else if (msg === 3){
+      row = arr[2].charAt(Math.floor(Math.random()*arr[2].length));
+    } else {
+      console.log("error");
+    }
+
+  return row;
+}
+console.log(getRandCharInRow());
+
 //Task3
-
-
 function getRandCharInAlph() {
 	let s;
-  const keyboard = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
-  s = keyboard.charAt(Math.floor(Math.random()*keyboard.length));
+  const alphArray = "qwertyuiop[]asdfghjkl;'zxcvbnm,./";
+  s = alphArray.charAt(Math.floor(Math.random()*alphArray.length));
 
   return s;
 
